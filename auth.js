5366e5b21a953b48cf009cd75dab8b1634f5471f
Rafal <rgrajcar@gmail.com>
2024-12-10 21:14:02 +0100

    added client initialization

--- auth.js
+++ auth.js
@@ -4,6 +4,8 @@ const serverless = require("serverless-http");
 const {body} = require("express-validator");
 const { CognitoIdentityProviderClient, ListDevicesCommand } = require("@aws-sdk/client-cognito-identity-provider");
 
+const client = new CognitoIdentityProviderClient({ region: "REGION" });
+
 const app = express();
 
 app.use(express.json());
