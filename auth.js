67011da1a0060f93ecf216c4e1c6add6a2e63beb
Rafal <rgrajcar@gmail.com>
2024-12-10 21:54:21 +0100

    added login params with AdminInitiateAuthCommand

--- auth.js
+++ auth.js
@@ -2,9 +2,9 @@ const express = require("express");
 const { validationResult } = require("express-validator");
 const serverless = require("serverless-http");
 const {body} = require("express-validator");
-const { CognitoIdentityProviderClient, SignUpCommand, ConfirmSignUpCommand } = require("@aws-sdk/client-cognito-identity-provider");
+const { CognitoIdentityProviderClient, SignUpCommand, ConfirmSignUpCommand, AdminInitiateAuthCommand } = require("@aws-sdk/client-cognito-identity-provider");
 
-const { REGION, CLIENT_ID } = process.env;
+const { REGION, CLIENT_ID, USER_POOL_ID } = process.env;
 const clientCognito = new CognitoIdentityProviderClient({ region: REGION });
 
 const app = express();
@@ -13,7 +13,43 @@ app.use(express.json());
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {
+router.post(
+  "/login", 
+  body('email').isEmail(), 
+  body('password').notEmpty(), 
+  async (req, res) => {
+    const result = validationResult(req);
+    if(!result.isEmpty()) {
+      return res.status(200).json({ errors: result.array() })
+    }
+
+    const params = {
+      AuthFlow: "ADMIN_NO_SRP_AUTH",
+      UserPoolId: USER_POOL_ID,
+      ClientId: CLIENT_ID,
+      AuthParameters: {
+        USERNAME: req.body.email,
+        PASSWORD: req.body.password,
+      }
+    }
+
+    try {
+      const command = new AdminInitiateAuthCommand(params);
+      const response = await clientCognito.send(command);
+
+      res.status(200).json({ sub: response.UserSub });
+
+    } catch(error) {
+      console.log(error);
+
+      res.status(500).json({ 
+        message: "Nie udało się zarejestrować użytkownika",
+        error: error.message, 
+      });
+    }
+
+    
+
     res.status(200).json({ message: "login" });
 });
 
